e2e068c671f46a200d6cc2a985a80e0a06328524
Igor Loncar <igor023@gmail.com>
2020-04-25 15:30:18 +0200

    reactive forms start, template driven forms wrap-up

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { RecipesService } from '../services/recipes.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.scss']
+  styleUrls: ['./recipe-edit.component.scss'],
 })
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipe: Recipe;
+  recipeForm: FormGroup;
 
   constructor(
     private recipesService: RecipesService,
@@ -22,11 +24,34 @@ export class RecipeEditComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-      this.recipe = this.recipesService.getRecipe(this.id);
+      if (this.editMode) {
+        this.recipe = this.recipesService.getRecipe(this.id);
+      }
+      this.initForm();
     });
   }
 
-  onSaveRecipe() {}
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      recipeName = this.recipe.name;
+      recipeImagePath = this.recipe.imagePath;
+      recipeDescription = this.recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 
   onCancel() {
     this.router.navigate(['..'], { relativeTo: this.route });
